2f3ca39af517fcdd05df6e6963c8b6cf8e6f778a
Erik P Almaraz <erikalmaraz@fastmail.com>
2025-05-17 19:50:38 -0700

    Scaffold new system for CL build expressions

---
+++ libraries/cl-bexp/bexp.lisp
@@ -0,0 +1,24 @@
+(defpackage :confer/libraries/cl-bexp/bexp
+  (:use :cl)
+  (:import-from :cl-interpol)
+  (:import-from :cl-ppcre
+                #:regex-replace)
+  (:import-from :local-time
+                #:now)
+  (:export #:test-fn))
+(in-package :confer/libraries/cl-bexp/bexp)
+
+;;; Notes:
+;;;
+;;; WIP - Goal to transcribe the base functionality of gexps to Common Lisp
+;;;       i.e. build expressions (b-expressions - bexp)
+;;;
+;;; To describe a derivation and its build actions, one typically needs to embed build
+;;; code inside host code. It boils down to manipulating build code as data, and the 
+;;; homoiconicity of lisp—code has a direct representation as data—comes in handy 
+;;; for that. But we need more than the normal quasiquote mechanism in lisp to 
+;;; construct build expressions.
+
+;;; ref: https://guix.gnu.org/manual/en/html_node/G_002dExpressions.html
+
+(defun test-fn ())
--- libraries/cl-gexp/all.lisp
+++
@@ -1,5 +0,0 @@
-(uiop:define-package :confer/libraries/cl-gexp/all
-  (:nicknames :gexp)
-  (:use :cl)
-  (:use-reexport
-   #:confer/libraries/cl-gexp/gexp))
